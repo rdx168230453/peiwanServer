529ebeb733d3f555055557df8e2ca3028e15396e
rdx <168230453@qq.com>
2019-06-13 16:48:35 +0800

    login

--- routes/login.js
+++ routes/login.js
@@ -31,8 +31,8 @@ router.post('/login',function(req, res){
         })    
     }).then((data)=>{
         //数据库存储
-        var sqlstr = 'insert into user(openid,session_key) values(?,?)'
-        var params=[data.openid,data.session_key]
+        var sqlstr = 'insert into user(openid,session_key) values(?,?) on duplicate key update session_key = ?' //检查更新保存
+        var params=[data.openid,data.session_key,data.session_key]
         db.query(sqlstr,params,function(error,res){
             if(error) throw error
             console.log(res)
@@ -44,11 +44,17 @@ router.post('/login',function(req, res){
     })
 })  
 
-router.get('/saveInfo',function(req,res){
-    console.log(req.body)
+router.post('/saveInfo',function(req,res){
+    var data = req.body
+    console.log(data)
+    var sql = 'update user set session_key=?,avatarUrl=?,city=?,country=?,gender=?,language=?,nickName=?,province=? where openid = ?'
+    var params = [data.session_key,data.avatarUrl,data.city,data.country,data.gender,data.language,data.nickName,data.province,data.openid]
+    db.query(sql,params,(error)=>{
+        if(error) throw error
+        res.send('提交成功')
+    })
 })
 router.post('/skillSubmit',function(req,res){
-    console.log(req.body)
     var body = req.body
     var sql = 'insert into examine() value(?,?,?,?,?,?);'
     var params=[body.id,body.type,body.skillimg,body.voiceintro,body.textintro,body.ranking]
@@ -61,11 +67,17 @@ router.get('/list',function(req,res){
     var sql = 'select * from examine'
     db.query(sql,(error,data)=>{
         if(error) throw errow
-        console.log(data)
         res.send({
             status:200,
             data:data
         })
     })
 })
+router.get('/getListTest',function(req,res){
+    console.log('123')
+    res.send({
+        status:200,
+        data:[1,2,4]
+    })
+})  
 module.exports = router
